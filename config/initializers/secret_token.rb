b181e2d1309af3e41a975ceef28df6511cf71462
kkamperschroer <kkamperschroer@gmail.com>
2012-05-20 13:48:33 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KylekMe::Application.config.secret_token = '3c9c70b0c8b174435b5f5874fa30a50706be0411b1184855d7a3f82c3c18a14573ab52cbad3626bfb06634d2bccdec5271b01360365b58fb852676c8595e8668'
